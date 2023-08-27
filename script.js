36aee90153f22a80489ed244fc22ada3453b2112
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-08-27 13:18:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// function updateTimer() {
+// 	const timerElement = document.getElementById("timer");
+// 	const time = new date();
+// }
+function updateTimer() {
+      const timerElement = document.getElementById('timer');
+      const now = new Date();
+      const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true };
+      const formattedTime = now.toLocaleDateString('en-US', options);
+      timerElement.textContent = formattedTime;
+    }
+
+    // Initial call to updateTimer to set the initial time
+    updateTimer();
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
